288359ba1a51b85c576a27f2da98b8dd5d374a88
aditya Kharade <adityakharade51@gmail.com>
2020-10-19 17:54:39 +0530

    Added : added functionality to write data to file

--- src/main/java/com/javanio/Employee.java
+++ src/main/java/com/javanio/Employee.java
@@ -4,17 +4,17 @@ public class Employee {
 	public String name;
 	public int id;
 	public double salary;
-	
+
 	public Employee(int id, String name, double salary) {
 		super();
 		this.name = name;
 		this.id = id;
 		this.salary = salary;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "name=" + name + ", id=" + id + ", salary=" + salary;
+		return "id=" + id + ", name=" + name + ", salary=" + salary;
 	}
-	
+
 }
